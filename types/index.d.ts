0f0eee7feb71c374937d91cc94069702430fb7e5
daniseifeddine <dany.a.seifeddine@gmail.com>
2025-03-17 00:51:32 +0200

    update

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,5 @@
 // Type definitions for lebify-ui
 // Project: https://github.com/lebify-ui
-// Definitions by: Your Name <https://github.com/yourusername>
 
 export * from './Button';
 export * from './ThemeProvider';
